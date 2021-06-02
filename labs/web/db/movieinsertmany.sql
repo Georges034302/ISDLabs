61e238fed4184ebe0c6afbaee666acbcb1b0a4ed
Georges034302 <georges.boughantous@gmail.com>
2021-06-02 08:58:53 +0100

    movie table

---
+++ labs/web/db/movieinsertmany.sql
@@ -0,0 +1,11 @@
+/**
+ * Author:  George
+ * Created: Jun 2, 2021
+ */
+INSERT INTO ISDUSER.MOVIE("NAME",GENRE,RATING,"YEAR",PRICE,URL)
+VALUES
+('The Dark Knight','Action, Crime, Drama',9,2008,12.8,'https://www.imdb.com/title/tt0468569/?ref_=ext_shr_lnk'),
+('Django Unchained','Drama, Western',8.4,2013,10.5,'https://www.imdb.com/title/tt1853728/?ref_=ext_shr_lnk'),
+('Interstellar','Adventure, Drama, Sci-Fi',8.6,2014,15.5,'https://www.imdb.com/title/tt0816692/?ref_=ext_shr_lnk'),
+('Red Dragon','Crime, Thrille',7.2,2002,9.50,'https://www.imdb.com/title/tt0289765/?ref_=ext_shr_lnk'),
+('Gran Torino','Drama',8.1,2008,12.5,'https://www.imdb.com/title/tt1205489/?ref_=ext_shr_lnk');
